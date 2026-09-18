a7e332a26809317fdc04fef15257e93690cea2c1
carlinbrr <carloscollaguazo5@gmail.com>
2026-09-18 10:18:34 +0200

    feature: add alb http listener

--- src/main/java/com/cdcollaguazo/infra/CdcollaguazoApp.java
+++ src/main/java/com/cdcollaguazo/infra/CdcollaguazoApp.java
@@ -2,21 +2,19 @@ package com.cdcollaguazo.infra;
 
 import software.amazon.awscdk.*;
 
-public class IacApp extends App {
+public class CdcollaguazoApp {
 
-    public IacApp() {
-        super(AppProps.builder().outdir("./cdk.out").build());
+    public static void main(String[] args) {
+        App app = new App(AppProps.builder().outdir("./cdk.out").build());
 
         // Add BootstraplessSynthesizer since we don't need to upload any assets
         // Only template creation is needed
         StackProps props = StackProps.builder().synthesizer(new BootstraplessSynthesizer()).build();
 
-        new CdcollaguazoStack(this, "Cdcollaguazo", props);
-    }
+        new CdcollaguazoStack(app, "Cdcollaguazo", props);
 
-    public static void main(String[] args) {
         System.out.println("Synthesizing cdcollaguazo infrastructure...");
-        new IacApp().synth();
+        app.synth();
     }
 
 }
--- src/main/java/com/cdcollaguazo/infra/construct/ComputeConstruct.java
+++ src/main/java/com/cdcollaguazo/infra/construct/ComputeConstruct.java
@@ -6,8 +6,7 @@ import software.amazon.awscdk.services.ec2.SubnetType;
 import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.services.ecs.ContainerInsights;
-import software.amazon.awscdk.services.elasticloadbalancingv2.ApplicationLoadBalancer;
-import software.amazon.awscdk.services.elasticloadbalancingv2.IpAddressType;
+import software.amazon.awscdk.services.elasticloadbalancingv2.*;
 import software.constructs.Construct;
 
 public class ComputeConstruct extends Construct {
@@ -35,6 +34,23 @@ public class ComputeConstruct extends Construct {
                         .build())
                 .securityGroup(albSg)
                 .build();
+
+        alb.addListener("HttpListener",
+                ApplicationListenerProps.builder()
+                        .loadBalancer(alb)
+                        .protocol(ApplicationProtocol.HTTP)
+                        .port(80)
+                        .open(false)
+                        .defaultAction(
+                                ListenerAction.fixedResponse(
+                                        404,
+                                        FixedResponseOptions.builder()
+                                                .contentType("text/plain")
+                                                .messageBody("Not Found")
+                                                .build()
+                                )
+                        )
+                        .build());
     }
 
     public ApplicationLoadBalancer getAlb() {
